dff93b4dd02956b5360195996e3e809e54a97137
Elena Yaroma <yaroma.elena@gmail.com>
2023-10-09 15:36:56 +0400

    feat: add the necessary dependencies and connect express

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log('Hello world!');
+import 'dotenv/config';
+import express from 'express';
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
